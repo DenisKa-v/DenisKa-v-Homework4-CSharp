85ed9bbcb044525e2c981e767c9535da61aad69a
DenisKa <pochta_dk@mail.ru>
2022-11-19 21:05:17 +0300

    Add task25

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
 ﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B (Math.Pow НЕ использовать)
+/* 
+int Exponentiation (int a, int b)
+ {
+    int multi = 1;
+    int count = 1;
+    while (count <= b)
+    {
+        count ++;
+        multi = multi * a;
+    }
+    return multi;
+ }
 
+Console.Write("Input number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input natural number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Number {a} to the extent {b} is {Exponentiation(a, b)}");
+ */
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 //Задача 29: Напишите программу, которая создает массив заданного пользователем размера, заполняет массив элементами от 1 до 99 и выводит их на экран.
